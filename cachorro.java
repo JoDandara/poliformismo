144274a833fc8dbb37d7ce3c5ac21d7a06b12e76
João Dandara <jdandajobs@gmail.com>
2021-12-17 00:54:01 -0300

    s

---
+++ cachorro.java
@@ -0,0 +1,14 @@
+
+  public class Cachorro extends Animal {	
+		
+		public void acao() {		
+			
+			System.out.println(" está correndo");
+		}
+		public void emitirSom() {
+
+			System.out.println(" e faz AU AU AU AU");
+		}
+
+	}
+
---
+++ cavalo.java
@@ -0,0 +1,13 @@
+public class Cavalo extends Animal {
+		
+		//@Override
+		public void acao() {		
+			
+			System.out.println(" está correndo");
+		}
+		public void emitirSom() {
+
+			System.out.println(" e está cavalgando");
+		}
+	}
+
